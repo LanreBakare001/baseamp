9e8f7e5c8a59f973e8db2800c5a16842d526f9f1
Lanre Bakare <Lanre.Bakare@aws.cstg.local>
2022-03-14 11:13:12 -0500

    Initial Commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "baseamp": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
